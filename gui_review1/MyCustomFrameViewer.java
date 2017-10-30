18aa8dd87f91ad30e07dfeec3e7c20912847926f
Andrew <andrewdurkiewicz@gmail.com>
2017-10-30 09:59:46 -0700

    organizing class structures

--- gui_review1/MyCustomFrameViewer.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-public class MyCustomFrameViewer
-{
-	public static void main(String[] args)
-	{
-		MyCustomFrame frame = new MyCustomFrame();
-		frame.setVisible(true);
-	}
-} 
